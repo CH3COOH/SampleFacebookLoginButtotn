b5e5c150da541381d3069da01bb6291a7a03d236
CH3COOH <ch3cooh393@gmail.com>
2020-03-30 20:31:53 +0900

    first commit

---
+++ SampleFacebookLoginButtotn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleFacebookLoginButtotn
+//
+//  Created by KENJI WADA on 2020/03/29.
+//  Copyright © 2020 ch3cooh.jp. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
